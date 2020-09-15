3b2c21decbcb98392cb5598ac64d15c34dc6733c
TRAN Thai hung <htran1@mantu.com>
2020-09-15 13:33:09 +0700

    -update

---
+++ CleanArch/CleanArch.Infa.Data/UniversityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infa.Data
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions options) : base (options)
+        {
+
+        }
+        DbSet<Course> Courses { get; set; }
+    }
+}
